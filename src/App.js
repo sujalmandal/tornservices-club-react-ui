018efad36d98079541e8016479c19826a54b2da2
Sujal <ss6sujal@gmail.com>
2021-03-30 18:14:07 +0530

    add search bar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import { JobSearchBar } from './features/job-search-bar/JobSearchBar';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
--- src/features/job-search-bar/JobSearchBar.js
+++ src/features/job-search-bar/JobSearchBar.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
+import { Navbar, Nav, NavDropdown, Form, FormControl, Button, Container } from 'react-bootstrap';
 import {
     searchJobs,
     selectGlobalJobFilters
@@ -15,46 +16,44 @@ export function JobSearchBar() {
 
     /* local, feature-level states */
     const [jobFilters, setJobFilters] = useState({
-        jobType: "BOUNTY",
+        jobType: "Bounty",
         amount: 0
     });
 
     return (
         <div>
-            <div>
-                <div>
-                    <span>Job Type</span>
-                </div>
-                <div>
-                    <select id="jobType" defaultValue={jobFilters.jobType}
-                        onChange={(e) => {
-                            setJobFilters({
-                                ...jobFilters,
-                                jobType: e.target.value
-                            })
-                        }}>
-                        <option value="BOUNTY">Bounty</option>
-                        <option value="HOSPITALIZE">Hospitalize</option>
-                        <option value="MUG">Mug</option>
-                        <option value="BOUNTY_REVEAL">Bounty Reveal</option>
-                    </select>
-                </div>
-            </div>
-            <div>
-                <div>
-                    <span>Amount</span>
-                </div>
-                <div>
-                    <input type="number" defaultValue={jobFilters.amount}
-                        onChange={(e) => {
-                            setJobFilters({
-                                ...jobFilters,
-                                amount: e.target.value
-                            })
-                        }} />
-                </div>
-            </div>
-            <div><button onClick={() => { dispatch(searchJobs(jobFilters))}}>Search Jobs</button></div>
+            <Navbar fixed="top" bg="dark" variant="dark" expand="lg">
+                <Container><Navbar.Brand href="#home">Find available jobs </Navbar.Brand></Container>
+                <Container>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="mr-auto">
+                            <NavDropdown title={"What kind?  " + jobFilters.jobType} id="basic-nav-dropdown" onSelect={(selectedItem) => {
+                                setJobFilters({
+                                    ...jobFilters,
+                                    jobType: selectedItem
+                                })
+                            }}>
+                                <NavDropdown.Item eventKey="Hospitalize">Hospitalize</NavDropdown.Item>
+                                <NavDropdown.Item eventKey="Mug">Mug</NavDropdown.Item>
+                                <NavDropdown.Divider />
+                                <NavDropdown.Item eventKey="Bounty reveal">Bounty reveal</NavDropdown.Item>
+                                <NavDropdown.Item eventKey="Bounty">Bounty</NavDropdown.Item>
+                            </NavDropdown>
+                        </Nav>
+                        <Form inline>
+                            <Form.Label className="mr-sm-4" style={{ color: "gray" }}>How many?</Form.Label>
+                            <FormControl style={{ width: "75px" }} max="99" value={jobFilters.amount} type="number" className="mr-sm-4" onChange={(e) => {
+                                setJobFilters({
+                                    ...jobFilters,
+                                    amount: e.target.value
+                                })
+                            }} />
+                            <Button onClick={() => { dispatch(searchJobs(jobFilters)) }} variant="outline-success" >Find!</Button>
+                        </Form>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
         </div>
     );
 
