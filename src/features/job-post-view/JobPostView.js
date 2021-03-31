59888292a313f46f6f6b89e03ab3efbed386abe5
Sujal <ss6sujal@gmail.com>
2021-03-31 22:23:54 +0530

    add event handlers in job create view

--- src/features/job-post-view/JobPostView.js
+++ src/features/job-post-view/JobPostView.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
-import { Button } from 'reactstrap';
+import { Button, Modal,Form, FormControl, Container } from 'react-bootstrap';
 import {
     postNewJob
 } from './JobPostViewSlice';
@@ -10,10 +10,30 @@ export function JobPostView() {
     const dispatch = useDispatch();
 
     const [showJobPostForm, setShowJobPostForm] = useState(false);
+    const [createJobDTO, setCreateJobDTO] = useState({
+        
+    });
 
     return (
         <div>
             <Button onClick={() => { setShowJobPostForm(true) }} variant="outline-success" >Post a new job</Button>
+            <Modal
+                show={showJobPostForm}
+                onHide={() => { setShowJobPostForm(false) }}
+                backdrop="static"
+                centered
+            >
+                <Modal.Header closeButton>
+                <Modal.Title>Modal title</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    
+                </Modal.Body>
+                <Modal.Footer>
+                <Button variant="secondary" onClick={() => { setShowJobPostForm(false) }}>Cancel</Button>
+                <Button variant="primary" onClick={()=>(dispatch(postNewJob(createJobDTO)))}>Post!</Button>
+                </Modal.Footer>
+            </Modal>
         </div>
     );
 
